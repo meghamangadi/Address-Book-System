df7c679311b92d1ed7b794c914f9327597357b74
megha angadi <meghamangadi@gmail.com>
2021-09-05 19:47:44 +0530

    Added: Program to edit the existing contact person using their name

--- src/com/blz/addressbooksystem/AddressBookMainClass.java
+++ src/com/blz/addressbooksystem/AddressBookMainClass.java
@@ -4,34 +4,98 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class AddressBookMainClass {
-	//public static List<Contact> contactList=new ArrayList<>();
-	//public static AddressBook addressBook=new AddressBook();
+public class AddressBookMainClass { 
+	public static AddressBook addressBook=new AddressBook();
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
 		System.out.println("Welcome to Address Book Program");
-		System.out.println("Enter the First Name");
-		String firstName = sc.next();
-		System.out.println("Enter the Last Name");
-		String lastName = sc.next();
-		System.out.println("Enter The Address");
-		String address = sc.next();
-		System.out.println("Enter the city");
-		String city = sc.next();
-		System.out.println("Enter the state");
-		String state = sc.next();
-		System.out.println("Enter the zip Code");
-		int zip = sc.nextInt();
-		System.out.println("Enter the Phone number");
-		long phone = sc.nextLong();
-		List<Contact> contactList=new ArrayList<>();
-		Contact contact= new Contact( firstName,   lastName,   address,   city,   state,   zip,   phone);
-	    AddressBook addressBook=new AddressBook();
-		contactList.add(contact); 
-		addressBook.setContacts(contactList);
-		displayContacts(addressBook);
+		addContactDetails();
+	}
+	
+	
+	public static void addContactDetails() {
+		
+
+		Scanner sc=new Scanner(System.in);
+		
+		
+		System.out.println("Enter your choice");
+		System.out.println("1 : Add new contact    2 : Edit contact   ");
+		int choice=sc.nextInt();
+		switch(choice)
+		{
+		case 1 :
+			System.out.println("Enter the First Name");
+			String firstName = sc.next();
+			System.out.println("Enter the Last Name");
+			String lastName = sc.next();
+			System.out.println("Enter The Address");
+			String address = sc.next();
+			System.out.println("Enter the city");
+			String city = sc.next();
+			System.out.println("Enter the state");
+			String state = sc.next();
+			System.out.println("Enter the zip Code");
+			int zip = sc.nextInt();
+			System.out.println("Enter the Phone number");
+			long phone = sc.nextLong();
+			List<Contact> contactList=new ArrayList<>();
+			Contact contact= new Contact( firstName,   lastName,   address,   city,   state,   zip,   phone);
+		    
+			contactList.add(contact); 
+			addressBook.setContacts(contactList);
+			displayContacts(addressBook);
+			addContactDetails();
+			break;
+		case 2:	
+
+			System.out.println("Enter the person Name");
+			String name = sc.next();
+			System.out.println("Enter the Last Name");
+			String editLastName = sc.next();
+			System.out.println("Enter The Address");
+			String editAddress = sc.next();
+			System.out.println("Enter the city");
+			String editCity = sc.next();
+			System.out.println("Enter the state");
+			String editState = sc.next();
+			System.out.println("Enter the zip Code");
+			int editzip = sc.nextInt();
+			System.out.println("Enter the Phone number");
+			long editPhone = sc.nextLong();
+			editContact(name,editLastName,editAddress,editCity,editState,editzip,editPhone);
+			addContactDetails();
+			break;
+			default :
+				System.out.println("Please Enter correct choice");
+				break;
+		
+		}
+		
+		
+		
 		sc.close();
 
+	
+	}
+	public static void editContact(String name,String lastname,String address,String city,String state,int zip,long phone) {
+		 List<Contact> contactDetails=addressBook.getContacts();
+		for (int i =0;i <=contactDetails.size()-1;i++) {
+			
+			Contact contactperson=contactDetails.get(i);
+			if (contactperson.getFirstName().equals(name)) {
+				
+				contactperson.setAddress(address);
+				contactperson.setCity(city);
+				contactperson.setFirstName(name);
+				contactperson.setLastName(lastname);
+				contactperson.setPhone(phone);
+				contactperson.setState(state);
+				contactperson.setZip(zip);
+				contactDetails.set(i, contactperson);
+				addressBook.setContacts(contactDetails);
+			}
+		}
+		displayContacts(addressBook);
 	}
 	 public static void displayContacts(AddressBook addressBook){
 		 List<Contact> contactDetails=addressBook.getContacts();
