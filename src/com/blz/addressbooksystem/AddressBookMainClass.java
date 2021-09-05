21d620b73f57ace02ee2a0b93f3044d0758fa90d
megha angadi <meghamangadi@gmail.com>
2021-09-05 17:22:51 +0530

    Added: Welcome Message to the User

---
+++ src/com/blz/addressbooksystem/AddressBookMainClass.java
@@ -0,0 +1,10 @@
+package com.blz.addressbooksystem;
+
+public class AddressBookMainClass {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program");
+
+	}
+
+}
